59fcea6702133f1a785d72d9556e6501c7d2c93d
Luke Warren <lukejkw@gmail.com>
2018-06-20 21:39:09 +0200

    add default port to server config

--- server/index.js
+++ server/index.js
@@ -5,4 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+const port = process.env.PORT;
+if (!port) {
+  console.error('No port found in environment');
+  process.exit(1);
+}
+
+app.listen(port)
+console.log(`Server Started: Running on localhost:${port}`)
