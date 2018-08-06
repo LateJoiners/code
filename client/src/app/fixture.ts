e01c496e0023c4f3085ffbb34a57d4e470e3c292
Matt Greig <mattg.vfx@gmail.com>
2018-08-07 08:29:03 +1000

    init definition of a fixture (or match)

---
+++ client/src/app/fixture.ts
@@ -0,0 +1,27 @@
+// Defines the properties on an individual match, or fixture
+
+import { Team } from './team';
+
+export class Fixture {
+  home: Team;
+  away: Team;
+  result: Array<number>;
+
+  constructor(home: Team, away: Team, result: Array<number>) {
+    this.home = home;
+    this.away = away;
+    this.result = result;
+  }
+
+  getVenue() {
+    return this.home.venue;
+  }
+
+  getHomeScore() {
+    return this.result[0];
+  }
+
+  getAwayScore() {
+    return this.result[1];
+  }
+}
