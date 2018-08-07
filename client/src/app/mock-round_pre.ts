52f3f819d0edad494dbc73242cfe1e1874b5a5dc
Matt Greig <mattg.vfx@gmail.com>
2018-08-07 23:49:29 +1000

    fixture data mocked up into rudimentary array  for use as a data source for a simulated round

--- client/src/app/mock-round_pre.ts
+++ client/src/app/mock-round_pre.ts
@@ -5,43 +5,47 @@ import { Team } from './team';
 import  * as team from './temp_data_teams';
 
 
-export const ROUND: Fixture[] = [
-  { home:
-      { id: 1, name_long: 'Manchester United', name_short: 'Man Utd', venue: 'Old Trafford' },
-    away:
-      { id: 2, name_long: 'Leicester City', name_short: 'LCY', venue: 'somewhere'},
-    result: [0, 0],
-    date: '10/8/2018'
+export let ROUND: Fixture[] = [];
+
+ROUND = [
+  { home: team.Man_Utd,
+  away: team.Leicester,
+  result: [0, 0],
+  date: '10/8/2018'
   },
-  { home:
-    { id: 3, name_long: 'AFC Bournemouth', name_short: 'AFC', venue: 'Old Trafford' },
-    away:
-      { id: 4, name_long: 'Arsenal', name_short: 'Arsenal', venue: 'Etihad Stadium'},
+  { home: team.AFC,
+    away: team.Cardiff,
     result: [0, 0],
     date: '11/8/2018'
   },
-  { home:
-    { id: 5, name_long: 'Huddersfield Town', name_short: 'Hud. Town', venue: 'somewhere' },
-    away:
-      { id: 6, name_long: 'Chelsea', name_short: 'Chelsea', venue: 'somewhere'},
+  { home: team.Fulham,
+    away: team.Crystal_Palace,
     result: [0, 0],
     date: '11/8/2018'
   },
-  { home:
-    { id: 7, name_long: 'Fulham', name_short: 'Fulham', venue: 'somewhere' },
-    away:
-      { id: 8, name_long: 'Crystal Palace', name_short: 'Cry Pal', venue: 'somewhere'},
+  { home: team.Huddersfield,
+    away: team.Chelsea,
     result: [0, 0],
     date: '11/8/2018'
   },
-  { home:
-    { id: 9, name_long: 'Newcastle United', name_short: 'New Utd', venue: 'somewhere' },
-    away:
-      { id: 10, name_long: 'Tottenham Hotspur', name_short: 'Spurs', venue: 'somewhere'},
+  { home: team.Newcastle,
+    away: team.Tottenham,
     result: [0, 0],
-    date: '10/8/2018'
+    date: '11/8/2018'
   },
-
-
-]
-
+  { home: team.Watford,
+    away: team.Brighton,
+    result: [0, 0],
+    date: '11/8/2018'
+  },
+  { home: team.Wolverhampton,
+    away: team.Everton,
+    result: [0, 0],
+    date: '11/8/2018'
+  },
+  { home: team.Arsenal,
+    away: team.Man_City,
+    result: [0, 0],
+    date: '12/8/2018'
+  },
+  ];
