7845004189ea1021c34bd3728779777180dc65fd
Matt Greig <mattg.vfx@gmail.com>
2018-08-07 23:45:25 +1000

    team data mocked up into rudimentary classes for use as a data source

---
+++ client/src/app/temp_data_teams.ts
@@ -0,0 +1,141 @@
+import { Team } from './team';
+
+export const Man_Utd: Team = {
+  id: 1,
+  name_long: 'Manchester United',
+  name_short: 'Man Utd',
+  venue: 'Old Trafford'
+}
+
+export const Leicester: Team = {
+  id: 2,
+  name_long: 'Leicester City',
+  name_short: 'LCY',
+  venue: 'unknown'
+}
+
+export const AFC: Team = {
+  id: 3,
+  name_long: 'AFC Bournemouth',
+  name_short: 'AFC',
+  venue: 'unknown'
+}
+
+export const Arsenal: Team = {
+  id: 4,
+  name_long: 'Arsenal',
+  name_short: 'Arsenal',
+  venue: 'unknown'
+}
+
+export const Fulham: Team = {
+  id: 5,
+  name_long: 'Fulham',
+  name_short: 'Fulham',
+  venue: 'unknown'
+}
+
+export const Crystal_Palace: Team = {
+  id: 6,
+  name_long: 'Crystal Palace',
+  name_short: 'CRY',
+  venue: 'unknown'
+}
+
+export const Huddersfield: Team = {
+  id: 7,
+  name_long: 'Huddersfield Town',
+  name_short: 'Huddersfield',
+  venue: 'unknown'
+}
+
+export const Chelsea: Team = {
+  id: 8,
+  name_long: 'Chelsea',
+  name_short: 'Chelsea',
+  venue: 'unknown'
+}
+
+export const Newcastle: Team = {
+  id: 9,
+  name_long: 'Newcastle United',
+  name_short: 'Newcastle Utd',
+  venue: 'unknown'
+}
+
+export const Tottenham: Team = {
+  id: 10,
+  name_long: 'Tottenham Hotspur',
+  name_short: 'Spurs',
+  venue: 'unknown'
+}
+
+export const Watford: Team = {
+  id: 11,
+  name_long: 'Watford',
+  name_short: 'Watford',
+  venue: 'unknown'
+}
+
+export const Brighton: Team = {
+  id: 12,
+  name_long: 'Brighton & Hove Albion',
+  name_short: 'Brighton',
+  venue: 'unknown'
+}
+
+export const Wolverhampton: Team = {
+  id: 13,
+  name_long: 'Wolverhampton Wanderers',
+  name_short: 'Wanderers',
+  venue: 'unknown'
+}
+
+export const Everton: Team = {
+  id: 14,
+  name_long: 'Everton',
+  name_short: 'Everton',
+  venue: 'unknown'
+}
+
+export const Cardiff: Team = {
+  id: 15,
+  name_long: 'Cardiff City',
+  name_short: 'Cardiff',
+  venue: 'unknown'
+}
+
+export const Man_City: Team = {
+  id: 16,
+  name_long: 'Manchester City',
+  name_short: 'Man City',
+  venue: 'unknown'
+}
+
+export const Liverpool: Team = {
+  id: 17,
+  name_long: 'Liverpool',
+  name_short: 'Liverpool',
+  venue: 'unknown'
+}
+
+export const West_Ham: Team = {
+  id: 18,
+  name_long: 'West Ham United',
+  name_short: 'West Ham',
+  venue: 'unknown'
+}
+
+export const Southampton: Team = {
+  id: 19,
+  name_long: 'Southampton',
+  name_short: 'Southampton',
+  venue: 'unknown'
+}
+
+export const Burnley: Team = {
+  id: 20,
+  name_long: 'Burnley',
+  name_short: 'Burnley',
+  venue: 'unknown'
+}
