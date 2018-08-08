adf8d152d3843f0aae407e3a1827c34fc25a9794
Matt Greig <mattg.vfx@gmail.com>
2018-08-08 17:42:44 +1000

    refactored Fixture class to be more fully featured

--- client/src/app/models/fixture.ts
+++ client/src/app/models/fixture.ts
@@ -3,29 +3,37 @@
 import { Team } from './team';
 
 export class Fixture {
-  id: number; // give each unique fixture a ref ID
+  id: string; // give each unique fixture a ref ID
   home: Team;
   away: Team;
-  result: Array<number>;
+  result: [number, number];
   date: string;
 
-  // constructor(home: Team, away: Team, result: Array<number>) {
-  //   this.home = home;
-  //   this.away = away;
-  //   this.result = result;
+
+  constructor(id: string,
+              home: Team,
+              away: Team,
+              result: [number, number],
+              date: string)
+  {
+    this.home = home;
+    this.away = away;
+    this.result = result;
+    this.date = date;
+
   // generate a random hash ID for the fixture based on date and teams playing
-  //
-  // }
-  //
-  // getVenue() {
-  //   return this.home.venue;
-  // }
-  //
-  // getHomeScore() {
-  //   return this.result[0];
-  // }
-  //
-  // getAwayScore() {
-  //   return this.result[1];
-  // }
+
+  }
+
+  getVenue(): string {
+    return this.home.venue;
+  }
+
+  getHomeScore(): number {
+    return this.result[0];
+  }
+
+  getAwayScore(): number {
+    return this.result[1];
+  }
 }
