55c90c1f9dbd91eede300c0ecda4089be07fdcb1
Matt Greig <mattg.vfx@gmail.com>
2018-08-16 22:24:51 +1000

    updated game service; still reporting an error

--- client/src/app/services/games.service.ts
+++ client/src/app/services/games.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Round } from '../data/round';
+import { Fixture } from '../models/fixture';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class GamesService {
 
   this_round = Round;
 
-  getRound(): Promise<Array<any>> {
+  getRound(): Promise<Array<Fixture>> {
     console.log('getRound called');
     return Promise.resolve(this.this_round);
   }
