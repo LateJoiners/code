1488ab60e14b8de3c498e09b81005cf139da76fd
Matt Greig <mattg.vfx@gmail.com>
2018-08-16 22:26:09 +1000

    sample dummy round with single fixture

---
+++ client/src/app/data/round_2.ts
@@ -0,0 +1,9 @@
+export const Round = [
+            {
+              "id": "0001",
+              "home": "Manchester United",
+              "away": "Leicester City",
+              "result": [0, 0],
+              "date": "10/8/2018"
+            }
+          ];
