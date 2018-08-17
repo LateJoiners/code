311184599bb385a9e86701f9f9593d9a0ec85e9e
Blair Fraser <s3641830@student.rmit.edu.au>
2018-08-18 00:45:57 +1000

    switch login to templatized version of mockup screen

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginAttemptFailed = false;
+
+  constructor() {
+    /** work out how to determine at this point whether
+    a login attempt has just failed, and if so execute the following:
+    this.loginAttemptFailed = true;
+    */
+  }
 
   ngOnInit() {
   }
