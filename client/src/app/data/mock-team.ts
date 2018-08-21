b0d1b4b661ed1ee4de18dd079e5c57a577bd66bd
Matt Greig <mattg.vfx@gmail.com>
2018-08-22 09:43:29 +1000

    created Team class and refactored mock Team data to better suit

--- client/src/app/data/mock-team.ts
+++ client/src/app/data/mock-team.ts
@@ -1,128 +1,124 @@
 // mock data to simulate team data
 
-export const TEAMS = [
-  {
-    'teams': [
+export const Teams = [
       {
-        'id': 1,
+        'id': '01',
         'name_long': 'Manchester United',
         'name_short': 'Man Utd',
         'venue': 'Old Trafford'
       },
       {
-        'id': 2,
+        'id': '02',
         'name_long': 'Leicester City',
         'name_short': 'LCY',
         'venue': 'King Power Stadium'
       },
       {
-        'id': 3,
+        'id': '03',
         'name_long': 'AFC Bournemouth',
         'name_short': 'AFC',
         'venue': 'Dean Court'
       },
       {
-        'id': 4,
+        'id': '04',
         'name_long': 'Arsenal',
         'name_short': 'Arsenal',
         'venue': 'Emirates Stadium'
       },
       {
-        'id': 5,
+        'id': '05',
         'name_long': 'Fulham',
         'name_short': 'Fulham',
         'venue': 'Craven Cottage'
       },
       {
-        'id': 6,
+        'id': '06',
         'name_long': 'Crystal Palace',
         'name_short': 'CRY',
         'venue': 'Selhurst Park'
       },
       {
-        'id': 7,
+        'id': '07',
         'name_long': 'Huddersfield Town',
         'name_short': 'Huddersfield',
         'venue': 'Kirklees Stadium'
       },
       {
-        'id': 8,
+        'id': '08',
         'name_long': 'Chelsea',
         'name_short': 'Chelsea',
         'venue': 'Stamford Bridge'
       },
       {
-        'id': 9,
+        'id': '09',
         'name_long': 'Newcastle United',
         'name_short': 'Newcastle Utd',
         'venue': 'St. James Park'
       },
       {
-        'id': 10,
+        'id': '10',
         'name_long': 'Tottenham Hotspur',
         'name_short': 'Spurs',
         'venue': 'Wembley Stadium'
       },
       {
-        'id': 11,
+        'id': '11',
         'name_long': 'Watford',
         'name_short': 'Watford',
         'venue': 'Vicarage Road'
       },
       {
-        'id': 12,
+        'id': '12',
         'name_long': 'Brighton & Hove Albion',
         'name_short': 'Brighton',
         'venue': 'Falmer Stadium'
       },
       {
-        'id': 13,
+        'id': '13',
         'name_long': 'Wolverhampton Wanderers',
         'name_short': 'Wanderers',
         'venue': 'Molineux Stadium'
       },
       {
-        'id': 14,
+        'id': '14',
         'name_long': 'Everton',
         'name_short': 'Everton',
         'venue': 'Goodison Park'
       },
       {
-        'id': 15,
+        'id': '15',
         'name_long': 'Cardiff City',
         'name_short': 'Cardiff',
         'venue': 'Cardiff City Stadium'
       },
       {
-        'id': 16,
+        'id': '16',
         'name_long': 'Manchester City',
         'name_short': 'Man City',
         'venue': 'Etihad Stadium'
       },
       {
-        'id': 17,
+        'id': '17',
         'name_long': 'Liverpool',
         'name_short': 'Liverpool',
         'venue': 'Anfield'
       },
       {
-        'id': 18,
+        'id': '18',
         'name_long': 'West Ham United',
         'name_short': 'West Ham',
         'venue': 'London Stadium'
       },
       {
-        'id': 19,
+        'id': '19',
         'name_long': 'Southampton',
         'name_short': 'Southampton',
         'venue': 'St. Marys Stadium'
       },
       {
-        'id': 20,
+        'id': '20',
         'name_long': 'Burnley',
         'name_short': 'Burnley',
         'venue': 'Turf Moor'
       }
-    ]
-  }
 ];
---
+++ client/src/app/models/team.ts
@@ -0,0 +1,6 @@
+export class Team {
+    id: string;
+    name_long: string;
+    name_short: string;
+    venue: string;
+}
