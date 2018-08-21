090bc39679aaf0029b96d81cc27bffcb9a81babd
Matt Greig <mattg.vfx@gmail.com>
2018-08-21 13:53:46 +1000

    moved mock-team data to data dir

---
+++ client/src/app/data/mock-team.ts
@@ -0,0 +1,128 @@
+// mock data to simulate team data
+
+export const TEAMS = [
+  {
+    'teams': [
+      {
+        'id': 1,
+        'name_long': 'Manchester United',
+        'name_short': 'Man Utd',
+        'venue': 'Old Trafford'
+      },
+      {
+        'id': 2,
+        'name_long': 'Leicester City',
+        'name_short': 'LCY',
+        'venue': 'King Power Stadium'
+      },
+      {
+        'id': 3,
+        'name_long': 'AFC Bournemouth',
+        'name_short': 'AFC',
+        'venue': 'Dean Court'
+      },
+      {
+        'id': 4,
+        'name_long': 'Arsenal',
+        'name_short': 'Arsenal',
+        'venue': 'Emirates Stadium'
+      },
+      {
+        'id': 5,
+        'name_long': 'Fulham',
+        'name_short': 'Fulham',
+        'venue': 'Craven Cottage'
+      },
+      {
+        'id': 6,
+        'name_long': 'Crystal Palace',
+        'name_short': 'CRY',
+        'venue': 'Selhurst Park'
+      },
+      {
+        'id': 7,
+        'name_long': 'Huddersfield Town',
+        'name_short': 'Huddersfield',
+        'venue': 'Kirklees Stadium'
+      },
+      {
+        'id': 8,
+        'name_long': 'Chelsea',
+        'name_short': 'Chelsea',
+        'venue': 'Stamford Bridge'
+      },
+      {
+        'id': 9,
+        'name_long': 'Newcastle United',
+        'name_short': 'Newcastle Utd',
+        'venue': 'St. James Park'
+      },
+      {
+        'id': 10,
+        'name_long': 'Tottenham Hotspur',
+        'name_short': 'Spurs',
+        'venue': 'Wembley Stadium'
+      },
+      {
+        'id': 11,
+        'name_long': 'Watford',
+        'name_short': 'Watford',
+        'venue': 'Vicarage Road'
+      },
+      {
+        'id': 12,
+        'name_long': 'Brighton & Hove Albion',
+        'name_short': 'Brighton',
+        'venue': 'Falmer Stadium'
+      },
+      {
+        'id': 13,
+        'name_long': 'Wolverhampton Wanderers',
+        'name_short': 'Wanderers',
+        'venue': 'Molineux Stadium'
+      },
+      {
+        'id': 14,
+        'name_long': 'Everton',
+        'name_short': 'Everton',
+        'venue': 'Goodison Park'
+      },
+      {
+        'id': 15,
+        'name_long': 'Cardiff City',
+        'name_short': 'Cardiff',
+        'venue': 'Cardiff City Stadium'
+      },
+      {
+        'id': 16,
+        'name_long': 'Manchester City',
+        'name_short': 'Man City',
+        'venue': 'Etihad Stadium'
+      },
+      {
+        'id': 17,
+        'name_long': 'Liverpool',
+        'name_short': 'Liverpool',
+        'venue': 'Anfield'
+      },
+      {
+        'id': 18,
+        'name_long': 'West Ham United',
+        'name_short': 'West Ham',
+        'venue': 'London Stadium'
+      },
+      {
+        'id': 19,
+        'name_long': 'Southampton',
+        'name_short': 'Southampton',
+        'venue': 'St. Marys Stadium'
+      },
+      {
+        'id': 20,
+        'name_long': 'Burnley',
+        'name_short': 'Burnley',
+        'venue': 'Turf Moor'
+      }
+    ]
+  }
+];
