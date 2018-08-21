22d50f7a77ae4425d6653f29a2a87fac58db18dc
Matt Greig <mattg.vfx@gmail.com>
2018-08-21 13:53:27 +1000

    test injecting league data into Sport object for pass to sports component

--- client/src/app/services/sports.service.ts
+++ client/src/app/services/sports.service.ts
@@ -2,17 +2,29 @@ import { Injectable } from '@angular/core';
 import { Sports } from '../data/sports';
 import { Sport } from '../models/sport';
 
+import { Football_Leagues } from '../data/football_leagues';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SportsService {
 
-  this_sports = Sports;
+  this_sports;
 
-  constructor() { }
+  constructor() {
+    this.this_sports = <Array<Sport>>Sports;
+    var sport_obj:Sport;
+    for (let sport_obj of this.this_sports) {
+
+      if (sport_obj.label == 'Football'){
+        sport_obj.leagues = Football_Leagues;
+      }
+    }
+   }
 
   getSports(): Promise<Array<Sport>> {
     console.log('getSports called');
+    console.log(this.this_sports);
     return Promise.resolve(this.this_sports);
   }
 }
