503ba3aaf4231dda53677b5cb3801d9c250cc153
Matt Greig <mattg.vfx@gmail.com>
2018-08-23 20:58:57 +1000

    init ladder data

---
+++ client/src/app/data/ladder.ts
@@ -0,0 +1,222 @@
+export const Ladder = [
+    {
+        'team_id': '01',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '02',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '03',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '04',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '05',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '06',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '07',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '08',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '09',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '10',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '11',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '12',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '13',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '14',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '15',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '16',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '17',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '18',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '19',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    },
+    {
+        'team_id': '20',
+        'stats': {
+            'played' : 0,
+            'wins' : 0,
+            'draws' : 0,
+            'losses' : 0,
+            'goals_for' : 0,
+            'goals_against' : 0,
+        }
+    }
+];
