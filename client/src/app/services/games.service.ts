60a1daade08837aa994673c0a1dcd59b55ad04a1
Matt Greig <mattg.vfx@gmail.com>
2018-08-23 12:28:12 +1000

    games now sorted by date

--- client/src/app/services/games.service.ts
+++ client/src/app/services/games.service.ts
@@ -11,10 +11,21 @@ export class GamesService {
 
   constructor() { }
 
-  this_round = Round;
+  round_one: Fixture[] = Round;
+
+  sortRoundData(): Fixture[] {
+    // this.sortedGames = this.games;
+    const gamesSort: Fixture[] = this.round_one.sort((leftSide, rightSide): number => {
+      if (leftSide.date < rightSide.date) { return -1; }
+      if (leftSide.date > rightSide.date) { return 1; }
+      return 0;
+    });
+    console.log('Sorted Games: ', gamesSort);
+    return gamesSort;
+  }
 
   getRound(): Promise<Array<Fixture>> {
     console.log('getRound called');
-    return Promise.resolve(this.this_round);
+    return Promise.resolve(this.sortRoundData());
   }
 }
