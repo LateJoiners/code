c65989a20f12560cde14737066ad17d907c4740c
Blair Fraser <s3641830@student.rmit.edu.au>
2018-08-23 10:57:11 +1000

    Make linter happy

--- client/src/app/account/account.component.ts
+++ client/src/app/account/account.component.ts
@@ -9,15 +9,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 })
 export class AccountComponent implements OnInit {
 
-  name = "Bob";
+  name = 'Bob';
   DOB = new Date();
-  username = "Bob";
-  email = "bob@somewhere.com";
+  username = 'Bob';
+  email = 'bob@somewhere.com';
   blankEmail = false;
-  //Was trying to use one variable w/ null to model all three states
-  //(no update attempted, update failed, update succeeded) but ran into
-  //odd behaviour most likely due to my lack of understanding on exactly how
-  //js/ts handles null - so going with simpler option (two booleans)
+  // Was trying to use one variable w/ null to model all three states
+  // (no update attempted, update failed, update succeeded) but ran into
+  // odd behaviour most likely due to my lack of understanding on exactly how
+  // js/ts handles null - so going with simpler option (two booleans)
   emailUpdateFailed = false;
   emailUpdateSuccess = false;
 
@@ -27,21 +27,21 @@ export class AccountComponent implements OnInit {
   }
 
   async attemptEmailUpdate() {
-    //presumably a call to a yet-to-be-written service that
-    //calls a yet-to-be-written API endpoint via post asking to
-    //update the email address, and receives back
+    // presumably a call to a yet-to-be-written service that
+    // calls a yet-to-be-written API endpoint via post asking to
+    // update the email address, and receives back
   }
 
   determineAction(something) {
-    //take result of attemptEmailUpdate and determine if attempt was successful
-    //(i.e. if emailUpdateSuccess should be set true, and setEmail() called)
-    //or unsuccessful (and determineErrorType() called)
+    // take result of attemptEmailUpdate and determine if attempt was successful
+    // (i.e. if emailUpdateSuccess should be set true, and setEmail() called)
+    // or unsuccessful (and determineErrorType() called)
   }
 
   determineErrorType(something) {
-    //work out what type of error (API complaining about blank email field,
-    //or some other error) and set emailUpdateFailed or blankEmail to be
-    //true as required
+    // work out what type of error (API complaining about blank email field,
+    // or some other error) and set emailUpdateFailed or blankEmail to be
+    // true as required
   }
 
   setEmail(email) {
--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
