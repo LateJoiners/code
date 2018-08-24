b2bc22ef974a7786ec895cf4e0d4e2d7481384e4
Matt Greig <mattg.vfx@gmail.com>
2018-08-24 10:55:46 +1000

    removed redundant data collection function

--- client/src/app/services/sports.service.ts
+++ client/src/app/services/sports.service.ts
@@ -27,22 +27,6 @@ export class SportsService {
     }
   }
 
-  getDataForLeague(): void {
-    // for each of the leagues in a sport
-    // attaches the related participation data
-    // for display
-
-    for (const sport_obj of this.sports) {
-      if (sport_obj.leagues) {
-        for (const tourney of sport_obj.leagues) {
-          const data = League_Data.find(i => i.id === tourney.id);
-          tourney.data = {'tournaments' : data.num_tournaments,
-                          'players' : data.num_players };
-        }
-      }
-    }
-  }
-
   getSports(): Promise<Array<Sport>> {
     console.log(this.sports);
     return Promise.resolve(this.sports);
