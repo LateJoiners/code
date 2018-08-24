fc7adddd988890718135d204157519291cd238c3
Matt Greig <mattg.vfx@gmail.com>
2018-08-24 10:54:32 +1000

    added a tournament 'activity' display to the sports view

---
+++ client/src/app/data/league_data.ts
@@ -0,0 +1,22 @@
+export const League_Data = [
+    {
+        'id': '0001',
+        'num_tournaments' : 13,
+        'num_players' : 167
+    },
+    {
+        'id': '0002',
+        'num_tournaments' : 7,
+        'num_players' : 123
+    },
+    {
+        'id': '0003',
+        'num_tournaments' : 9,
+        'num_players' : 83
+    },
+    {
+        'id': '0004',
+        'num_tournaments' : 5,
+        'num_players' : 42
+    }
+];
--- client/src/app/services/sports.service.ts
+++ client/src/app/services/sports.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Football_Leagues } from "../data/football_leagues";
+import { League_Data } from "../data/league_data";
 import { Sports } from "../data/sports";
 import { Sport } from "../models/sport";
+
 
 @Injectable({
   providedIn: "root"
@@ -15,10 +17,34 @@ export class SportsService {
       if (sport_obj.label === 'Football') {
         sport_obj.leagues = Football_Leagues;
       }
+      if (sport_obj.leagues) {
+        for (const tourney of sport_obj.leagues) {
+          const data = League_Data.find(i => i.id === tourney.id);
+          tourney.data = {'tournaments' : data.num_tournaments,
+                          'players' : data.num_players };
+        }
+      }
+    }
+  }
+
+  getDataForLeague(): void {
+    // for each of the leagues in a sport
+    // attaches the related participation data
+    // for display
+
+    for (const sport_obj of this.sports) {
+      if (sport_obj.leagues) {
+        for (const tourney of sport_obj.leagues) {
+          const data = League_Data.find(i => i.id === tourney.id);
+          tourney.data = {'tournaments' : data.num_tournaments,
+                          'players' : data.num_players };
+        }
+      }
     }
   }
 
   getSports(): Promise<Array<Sport>> {
+    console.log(this.sports);
     return Promise.resolve(this.sports);
   }
 }
