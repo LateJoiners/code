e03a0bf9fe49667902c1f534f01caa3ba2546c28
Blair Fraser <s3641830@student.rmit.edu.au>
2018-08-26 13:35:34 +1000

    Switch back to providing mock data directly from results service as using API was failing in deployment environment

--- client/src/app/services/results.service.ts
+++ client/src/app/services/results.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Tips } from '../data/mock-tips';
+import { Results } from '../data/mock-results';
 import { Observable, of } from 'rxjs';
+import { Tip } from '../models/tip';
+import { Fixture } from '../models/fixture';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -13,11 +17,32 @@ export class ResultsService {
 
   constructor(private http: HttpClient) { }
 
-  getTips() {
-    return this.http.get(`${this.uri}/tips/mock`);
+  getTips(): Observable<Tip[]> {
+
+    // Issues on deployment - "mixed active content"
+    // seems despite the uri variable having http*S*
+    // things are trying to use http - and browsers
+    // don't want to render something coming from
+    // both sources
+
+    // reverting to static files served directly through
+    // this service
+
+    // return this.http.get(`${this.uri}/tips/mock`);
+    return of(Tips);
   }
 
-  getResults() {
-    return this.http.get(`${this.uri}/results/mock`);
+  getResults(): Observable<Fixture[]> {
+    // Issues on deployment - "mixed active content"
+    // seems despite the uri variable having http*S*
+    // things are trying to use http - and browsers
+    // don't want to render something coming from
+    // both sources
+
+    // reverting to static files served directly through
+    // this service
+
+    // return this.http.get(`${this.uri}/results/mock`);
+    return of(Results);
   }
 }
